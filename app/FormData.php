52133496215c00907caab3d9271d7487910822aa
Andrey Sushko <andirpal@mail.ru>
2018-02-13 22:56:49 +0300

    Fixed FormData model

--- app/FormData.php
+++ app/FormData.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class FormData extends Model
 {
     protected $table = 'form_data';
-    protected $guarded = [];
+    protected $fillable = ['first_name', 'last_name', 'email', 'telegram_id', 'country', 'amount_participate', 'favorite_cryptocurrency', 'hear_source', 'user_id'];
 
     public function user()
     {
